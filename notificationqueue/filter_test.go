bc70937dfb10431c7efc00dd0644ed9376a045cb
jarlyyn <jarlyyn@qq.com>
2021-01-12 22:17:16 +0800

    update

--- notificationqueue/filter_test.go
+++ notificationqueue/filter_test.go
@@ -111,4 +111,124 @@ func TestFilter(t *testing.T) {
 	if !ok || err != nil {
 		t.Fatal(ok, err)
 	}
+
+	f = NewFilter()
+	err = ApplyToFilter(f, []*Condition{&Condition{Keyword: ConditionDelivery, Value: "12345"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if f.Delivery != "12345" {
+		t.Fatal(f)
+	}
+	n = notification.New()
+	ok, err = f.FilterNotification(n, 100000)
+	if ok || err != nil {
+		t.Fatal(ok, err)
+	}
+	n.Delivery = "12345"
+	ok, err = f.FilterNotification(n, 100000)
+	if !ok || err != nil {
+		t.Fatal(ok, err)
+	}
+	n.Delivery = "123456"
+	ok, err = f.FilterNotification(n, 100000)
+	if ok || err != nil {
+		t.Fatal(ok, err)
+	}
+
+	f = NewFilter()
+	err = ApplyToFilter(f, []*Condition{&Condition{Keyword: ConditionNotificationID, Value: "12345"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if f.NotificationID != "12345" {
+		t.Fatal(f)
+	}
+	n = notification.New()
+	ok, err = f.FilterNotification(n, 100000)
+	if ok || err != nil {
+		t.Fatal(ok, err)
+	}
+	n.ID = "12345"
+	ok, err = f.FilterNotification(n, 100000)
+	if !ok || err != nil {
+		t.Fatal(ok, err)
+	}
+	n.ID = "123456"
+	ok, err = f.FilterNotification(n, 100000)
+	if ok || err != nil {
+		t.Fatal(ok, err)
+	}
+
+	f = NewFilter()
+	err = ApplyToFilter(f, []*Condition{&Condition{Keyword: ConditionBatch, Value: "12345"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if f.BatchID != "12345" {
+		t.Fatal(f)
+	}
+	n = notification.New()
+	ok, err = f.FilterNotification(n, 100000)
+	if ok || err != nil {
+		t.Fatal(ok, err)
+	}
+	n.Header.Set(notification.HeaderNameBatch, "12345")
+	ok, err = f.FilterNotification(n, 100000)
+	if !ok || err != nil {
+		t.Fatal(ok, err)
+	}
+	n.Header.Set(notification.HeaderNameBatch, "123456")
+	ok, err = f.FilterNotification(n, 100000)
+	if ok || err != nil {
+		t.Fatal(ok, err)
+	}
+
+	f = NewFilter()
+	err = ApplyToFilter(f, []*Condition{&Condition{Keyword: ConditionTopic, Value: "12345"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if f.Topic != "12345" {
+		t.Fatal(f)
+	}
+	n = notification.New()
+	ok, err = f.FilterNotification(n, 100000)
+	if ok || err != nil {
+		t.Fatal(ok, err)
+	}
+	n.Header.Set(notification.HeaderNameTopic, "12345")
+	ok, err = f.FilterNotification(n, 100000)
+	if !ok || err != nil {
+		t.Fatal(ok, err)
+	}
+	n.Header.Set(notification.HeaderNameTopic, "123456")
+	ok, err = f.FilterNotification(n, 100000)
+	if ok || err != nil {
+		t.Fatal(ok, err)
+	}
+
+	f = NewFilter()
+	err = ApplyToFilter(f, []*Condition{&Condition{Keyword: ConditionTarget, Value: "12345"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if f.Target != "12345" {
+		t.Fatal(f)
+	}
+	n = notification.New()
+	ok, err = f.FilterNotification(n, 100000)
+	if ok || err != nil {
+		t.Fatal(ok, err)
+	}
+	n.Header.Set(notification.HeaderNameTarget, "12345")
+	ok, err = f.FilterNotification(n, 100000)
+	if !ok || err != nil {
+		t.Fatal(ok, err)
+	}
+	n.Header.Set(notification.HeaderNameTarget, "123456")
+	ok, err = f.FilterNotification(n, 100000)
+	if ok || err != nil {
+		t.Fatal(ok, err)
+	}
 }
